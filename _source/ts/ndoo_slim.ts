3fc01e94c6e8cee0472659be3d2ec1c3d9012f6b
lcc <leftcold@gmail.com>
2016-10-27 00:12:57 +0800

    add app class

--- _source/ts/ndoo_slim.ts
+++ _source/ts/ndoo_slim.ts
@@ -1,5 +1,15 @@
 import { Main } from './src/main';
+import { App } from './src/app';
+import { applyMixins } from './src/util';
 let lib = {};
-let ndoo = new Main(lib);
 
+class Slim extends Main {
+    constructor(lib) {
+        super(lib);
+    }
+}
+
+applyMixins(Slim, [App]);
+
+let ndoo = new Slim(lib);
 export default ndoo;
