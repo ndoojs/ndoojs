c28b02c8aa942e1df1243afa26950b1d7e382187
lcc <leftcold@gmail.com>
2017-04-17 19:55:23 +0800

    add types file

---
+++ _source/ts/ndoo.d.ts
@@ -0,0 +1,67 @@
+declare interface NdooFuncStatic {
+  [id: string]: any;
+}
+declare enum NdooRegType {
+  App,
+  Block,
+  Service
+}
+declare interface NdooBase {
+  [id: string]: any;
+  _isDebug: boolean;
+  pageId: string;
+
+  getPk: (prefix?: string) => string;
+  storage: {
+    (key: string): any;
+    <T>(key: string, value: T): T;
+    (key: string, value: any, option: number): any;
+    REWRITE: number;
+    DESTROY: number;
+  };
+
+  require: (depend: string[], callback: Function, type: 'seajs' | 'do') => any;
+
+  on: (eventName: string, callback: Function) => void;
+  off: (eventName: string) => void;
+  trigger: (eventName: string, ...param) => void;
+
+  app: {
+    (namespace: string): any;
+    <T>(namespace: string): T;
+    <T>(namespace: string, controller: T): T;
+  }
+  hasApp: (namespace: string) => boolean;
+  setApp: (namespace: string) => boolean;
+
+  block: {
+    (namespace: string): any;
+    <T>(namespace: string): T;
+    <T>(namespace: string, block: T): T;
+  }
+  hasBlock: (namespace: string) => boolean;
+  setBlock: (namespace: string) => boolean;
+  initBlock: (elem: any) => void;
+
+  service: {
+    (namespace: string): any;
+    <T>(namespace: string): T;
+    <T>(namespace: string, service: T): T;
+  }
+
+  init: (id?: string, depend?: string[]) => void;
+
+  func: NdooFuncStatic;
+
+  vars: any;
+  
+  RegType: NdooRegType;
+
+  Component: (path: string, type: RegType, isStatic = false) => (component: any) => void;
+}
+
+declare var ndoo: NdooBase;
+
+declare module "ndoo" {
+  export = ndoo;
+}
